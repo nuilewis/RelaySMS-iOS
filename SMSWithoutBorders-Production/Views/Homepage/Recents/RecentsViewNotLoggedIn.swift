2086ce737e7cffb53f006970bf0d4fbf7b9518fc
sherlock <wisdomnji@gmail.com>
2025-03-11 13:04:05 +0100

    update: fixed text for message compose

--- SMSWithoutBorders-Production/Views/Homepage/Recents/RecentsViewNotLoggedIn.swift
+++ SMSWithoutBorders-Production/Views/Homepage/Recents/RecentsViewNotLoggedIn.swift
@@ -13,18 +13,23 @@ struct ComposeNewMessageSheetView: View {
 
     var body: some View {
         VStack {
-            Image(systemName: "person.crop.circle.badge.plus")
+            Image(systemName: "plus.message")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 75, height: 75)
 
-            Text("This is a brief summary of what happens")
-                .multilineTextAlignment(.center)
-                .font(.title2)
+            Text("Message with RelaySMS accout")
+                .font(RelayTypography.titleLarge)
                 .multilineTextAlignment(.center)
                 .padding()
 
-            Text("This is precise information for what happens")
+            Text("You can send messages from RelaySMS at anytime. Your account details would be your phone number attached to our official domain.\n\nExample +123456789@relaysms.me")
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 8)
+            
+            Text("Replies would come back by SMS to the phone number associated with your account")
+                .multilineTextAlignment(.center)
+                .font(.footnote)
 
             Button(action: {
                 parentSheetShown.toggle()
@@ -35,7 +40,15 @@ struct ComposeNewMessageSheetView: View {
             .buttonStyle(.relayButton(variant: .primary))
             .tint(.primary)
             .padding()
+            
+            Text("Emailing is the only currently supported protocol, we are working on including more soon")
+                .multilineTextAlignment(.center)
+                .font(.footnote)
+                .foregroundStyle(.gray)
+                .padding(.bottom, 10)
+
         }
+        .padding([.leading, .trailing], 16)
     }
 }
 
@@ -55,7 +68,8 @@ struct CreateAccountSheetView: View {
                 .multilineTextAlignment(.center)
                 .padding()
 
-            Text("With an account you can save your online platforms to use them without an internet connection.").multilineTextAlignment(.center)
+            Text("With an account you can save your online platforms to use them without an internet connection.")
+                .multilineTextAlignment(.center)
             Spacer().frame(height: 32)
             Button(action: {
                 parentSheetShown.toggle()
