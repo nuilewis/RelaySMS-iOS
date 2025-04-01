3943fb36cf170fb416b85e4639be974d18a0aa08
Lewis <lewisnuikweh@gmail.com>
2025-04-01 15:12:36 +0100

    gateway client: add context menu to card for edit and delete options

--- SMSWithoutBorders-Production/Views/GatewayClients/Components/GatewayClientCard.swift
+++ SMSWithoutBorders-Production/Views/GatewayClients/Components/GatewayClientCard.swift
@@ -8,29 +8,65 @@
 import SwiftUI
 
 struct GatewayClientCard: View {
-    var selectedGatewayClient: GatewayClientsEntity
+    var clientEntity: GatewayClientsEntity
     var disabled: Bool
+    @State var showSheet: Bool = false
+    @State var isSuccessful: Bool = false
+    @State var editGatewayClientSheetPresented: Bool = false
+    @Environment(\.managedObjectContext) var context
 
     var body: some View {
         VStack {
             Group {
-                Text(selectedGatewayClient.msisdn!)
+                Text(clientEntity.msisdn ?? "N/A")
                     .font(.headline)
                     .padding(.bottom, 5)
-                    .foregroundColor(disabled ? .secondary : .primary )
+                    .foregroundColor(disabled ? .secondary : .primary)
 
                 HStack {
-                    Text(selectedGatewayClient.operatorName! + " -")
-                    Text(selectedGatewayClient.operatorCode!)
+                    Text(clientEntity.operatorName ?? "N/A" + " -")
+                    Text(clientEntity.operatorCode ?? "N/A")
                 }
                 .foregroundColor(.secondary)
                 .font(.subheadline)
 
-                Text(selectedGatewayClient.country!)
+                Text(clientEntity.country ?? "N/A")
                     .font(.caption)
                     .foregroundColor(.secondary)
             }
             .frame(maxWidth: .infinity, alignment: .leading)
+            .contextMenu {
+                Button("Edit", systemImage: "pencil") {
+                    editGatewayClientSheetPresented = true
+                    // Edit gateway Client
+                }
+                Button("Delete", systemImage: "trash", role: .destructive) {
+                    do {
+                        try GatewayClients.deleteGatewayClient(context: context, client: GatewayClients.fromEntity(entity: clientEntity))
+                        showSheet = true
+                        isSuccessful = true
+                    } catch {
+                        print("Unable to delete GatewayClient")
+                        showSheet = false
+                    }
+
+                }
+    
+            }.alert(
+                isPresented: $showSheet,
+                content: {
+                    Alert(title: Text("Gateway Client Deleted"))
+                })
+            .sheet(
+                isPresented: $editGatewayClientSheetPresented,
+                onDismiss: {
+                    editGatewayClientSheetPresented = false
+                },
+                content: {
+                    AddEditGatewayClientForm(
+                        gatewayClient: GatewayClients.fromEntity(entity: clientEntity),
+                        isPresented: $editGatewayClientSheetPresented)
+                })
         }
     }
 }
