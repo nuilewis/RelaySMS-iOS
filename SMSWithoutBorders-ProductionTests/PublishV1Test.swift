04a07dce3971cd9b3c157a6f4c2892d594ccb12d
Lewis <lewisnuikweh@gmail.com>
2025-04-18 16:17:49 +0100

    publish: continue test for v1 publish, format payload with tokens

--- SMSWithoutBorders-ProductionTests/PublishV1Test.swift
+++ SMSWithoutBorders-ProductionTests/PublishV1Test.swift
@@ -28,7 +28,7 @@ public class PublishV1Test: XCTestCase {
     
     func testSignInUserAndPublishV1ShouldPublish() throws {
         
-        // Arrange
+        /// Arrange
         /// Assume the user has already signed in.
         /// Migrate their platforms to the device
         /// Assert that the platforms exist
@@ -37,55 +37,61 @@ public class PublishV1Test: XCTestCase {
         /// construct a payload for publisher v1
         ///
         
-        // This assumes the user is already signed in
-        // log in with a demo account and get the llt token (the token between the user and relay)
+        /// This assumes the user is already created and has already added the Gmail platfom to their account
         
 
+        // Arrange
         let vault: Vault = Vault()
         let context: NSManagedObjectContext = makeInMemoryManagedObjectContext()
-        
-        //Sign In
-        var authenticationResponse = try  vault.authenticateEntity(context: context, phoneNumber: "+237000011111", password: "ABCdef124!")
-    
-        // Verify code
-        authenticationResponse = try vault.authenticateEntity(context: context, phoneNumber: "+237000011111", password: "ABCdef124!", ownershipResponse: "123456")
-
         let publisher = Publisher()
         
+        // Authenticate : Sign in, verify account and get LLT
+        var authenticationResponse = try  vault.authenticateEntity(context: context, phoneNumber: "+237000011111", password: "ABCdef124!")
+        authenticationResponse = try vault.authenticateEntity(context: context, phoneNumber: "+237000011111", password: "ABCdef124!", ownershipResponse: "123456")
+        
         XCTAssertNotNil(authenticationResponse.longLivedToken, "Long lived token should not be null")
-        print("auth response ltt: \(String(describing: authenticationResponse.longLivedToken))")
+        XCTAssertFalse(authenticationResponse.longLivedToken == "", "Long lived token should not be empty")
         let ltt = authenticationResponse.longLivedToken
-        print(ltt)
-
-
-//        let ltt = try Vault.getLongLivedToken()
-//        XCTAssertNotNil(ltt, "Long lived token from keychain should not be null")
-//
+       
+        
+        // Get Platform tokens from vault
         let response = try vault.listStoredEntityToken(longLiveToken: ltt, migrateToDevice: true)
         response.storedTokens.forEach { token in
             XCTAssertNotNil(token)
             XCTAssertTrue(token.isStoredOnDevice, "is stored on device should be true")
         }
         
+        // Extract Tokens
+        let gmailPlatformToken = response.storedTokens.first { token in token.platform == "gmail"}
+        XCTAssertNotNil(gmailPlatformToken, "gmail platform token should not be null")
+        var gAccessToken: String = ""
+        var gRefreshToken: String = ""
         
+        if let accountTokens = gmailPlatformToken?.accountTokens {
+            gAccessToken = accountTokens["access_token"]!
+            gRefreshToken = accountTokens["refresh_token"]!
+            XCTAssertFalse(gAccessToken == "", "Access token should not be empty")
+            XCTAssertFalse(gRefreshToken == "", "Refresh token should not be empty")
+        }
+    
         
         // Publish
-       let publishMessageComposer = try Publisher.publishV1(context: context)
-//        publishMessageComposer.messageComposerV1(platform_letter: "g".utf8.first, sender: "237000000000", receiver: "lewisnuikweh@gmail.com", message: "Test message")
+        // 1. Compose email
+        let publishMessageComposer = try Publisher.publishV1(context: context)
         
-        // Message
         let fromNumber: String = "237000000000"
         let to: String = "lewisnuikweh@gmail.com"
         let cc: String = ""
         let bcc: String = ""
         let subject: String = "Test Email"
         let message: String = "Hello World"
-        
         let platfomLetter = "g".utf8.first!
         
-        let composeEmaileResponse = try  publishMessageComposer.emailComposerV1(platform_letter: "g".utf8.first!, from: fromNumber, to: to, cc: cc, bcc: bcc, subject: subject, body: message)
+        let composeEmailResponse = try publishMessageComposer.emailComposerV1(platform_letter: platfomLetter, from: fromNumber, to: to, cc: cc, bcc: bcc, subject: subject, body: message, accessToken: gAccessToken, refreshToken:  gRefreshToken)
         
-        XCTAssertNotNil(composeEmaileResponse, "Compose email response should not be null")
+        XCTAssertNotNil(composeEmailResponse, "Compose email response should not be null")
+        
+        // 2. Trigger publish
         
         
     }
