79e8c440774d3c10b058d9b2b18f2887017fba20
Lewis <lewisnuikweh@gmail.com>
2025-04-25 16:01:47 +0100

    token storage: add missing token error alert for edge case if tokens are missing & account revocation
    This commit introduces an alert to inform the user when tokens are missing for a platform. It also adds the functionality to revoke platform access directly from the alert.
    
    - Implemented an alert that displays when tokens are missing for a given platform.
    - Added an account revocation option within the alert, allowing users to revoke platform access directly.

--- SMSWithoutBorders-Production/Views/PlatformComposeViews/TextComposeView.swift
+++ SMSWithoutBorders-Production/Views/PlatformComposeViews/TextComposeView.swift
@@ -5,25 +5,30 @@
 //  Created by Sherlock on 11/4/22.
 //
 
-import SwiftUI
 import MessageUI
+import SwiftUI
 
 struct TextComposeView: View {
     @Environment(\.dismiss) var dismiss
     @Environment(\.managedObjectContext) var context
     @Environment(\.presentationMode) var presentationMode
-    
+
     @FetchRequest var platforms: FetchedResults<PlatformsEntity>
     @FetchRequest var storedPlatforms: FetchedResults<StoredPlatformsEntity>
 
     #if DEBUG
-    private var defaultGatewayClientMsisdn: String =
-    ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" ? "" : UserDefaults.standard.object(forKey: GatewayClients.DEFAULT_GATEWAY_CLIENT_MSISDN) as? String ?? ""
+        private var defaultGatewayClientMsisdn: String =
+            ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"]
+                == "1"
+            ? ""
+            : UserDefaults.standard.object(
+                forKey: GatewayClients.DEFAULT_GATEWAY_CLIENT_MSISDN) as? String
+                ?? ""
     #else
         @AppStorage(GatewayClients.DEFAULT_GATEWAY_CLIENT_MSISDN)
         private var defaultGatewayClientMsisdn: String = ""
     #endif
-    
+
     @AppStorage(SettingsKeys.SETTINGS_STORE_PLATFORMS_ON_DEVICE)
     private var isPlatformsStoredOnDevice: Bool = false
 
@@ -34,14 +39,19 @@ struct TextComposeView: View {
     @State private var encryptedFormattedContent = ""
     @State private var isPosting: Bool = false
     @State private var isShowingMessages: Bool = false
-    
+
     @State private var dismissRequested: Bool = false
     @State private var requestToChooseAccount: Bool = false
 
     @State var platform: PlatformsEntity?
-    
+
     @Binding var message: Messages?
     @Binding var platformName: String
+
+    @State private var showAlert: Bool = false
+    @State private var alertTitle: String = ""
+    @State private var alertMessage: String = ""
+    @State private var isLoading = false
 
     init(platformName: Binding<String>, message: Binding<Messages?>) {
         _platformName = platformName
@@ -50,7 +60,7 @@ struct TextComposeView: View {
         _storedPlatforms = FetchRequest<StoredPlatformsEntity>(
             sortDescriptors: [],
             predicate: NSPredicate(format: "name == %@", platformNameWrapped))
-        
+
         _platforms = FetchRequest<PlatformsEntity>(
             sortDescriptors: [],
             predicate: NSPredicate(format: "name == %@", platformNameWrapped))
@@ -68,12 +78,21 @@ struct TextComposeView: View {
                         .multilineTextAlignment(.center)
                         .padding()
                 }
+
+                #if DEBUG
+                    Button("Present missing token error alert") {
+                        showAlert = true
+                        alertTitle = "Tokens Missing"
+                        alertMessage = "Your tokens have not been found on this device. Please revoke access to your account and log back in to continue."
+                    }
+                #endif
+
                 ZStack {
                     if self.textBody.isEmpty {
                         TextEditor(text: $placeHolder)
-                                .font(.body)
-                                .foregroundColor(.secondary)
-                                .disabled(true)
+                            .font(.body)
+                            .foregroundColor(.secondary)
+                            .disabled(true)
                     }
                     TextEditor(text: $textBody)
                         .font(.body)
@@ -83,13 +102,13 @@ struct TextComposeView: View {
             }
             .padding()
             .sheet(isPresented: $requestToChooseAccount) {
-                AccountSheetView(
+                SelectAccountSheetView(
                     filter: platformName,
                     fromAccount: $fromAccount,
                     dismissParent: $dismissRequested
                 ) {
                     requestToChooseAccount.toggle()
-                    
+
                     if self.message != nil {
                         textBody = self.message!.data
                     }
@@ -118,44 +137,54 @@ struct TextComposeView: View {
                     isPosting = true
                     DispatchQueue.background(background: {
                         do {
-                            let messageComposer = try Publisher.publish( context: context)
+                            let messageComposer = try Publisher.publish(context: context)
                             let tokenManager = StoredTokensEntityManager(context: context)
                             var shortcode: UInt8? = nil
                             shortcode = platform.shortcode!.bytes[0]
-                            
+
                             // Get the stored platform and use the tokens if the platform tokens exist
-                            let storedPlatformEntity = storedPlatforms.first {$0.account == fromAccount} // Gets the speciic account that matches the currently selected `fromAccount`
+                            let storedPlatformEntity = storedPlatforms.first {
+                                $0.account == fromAccount
+                            }  // Gets the speciic account that matches the currently selected `fromAccount`
                             var tokensExists: Bool = false
                             var storedTokenForPlatform: StoredToken?
-                            
+
                             if let entity = storedPlatformEntity, entity.isStoredOnDevice {
-                                print("Platform is stored on device.. Will use saved tokens for publishing if tokens are available")
+                                print(
+                                    "Platform is stored on device.. Will use saved tokens for publishing if tokens are available"
+                                )
                                 tokensExists = tokenManager.storedTokenExists(forPlarform: entity.id ?? "")
-                                
+
                                 // Get tokens if they exist
                                 if tokensExists {
                                     storedTokenForPlatform = tokenManager.getStoredToken(forPlatform: entity.id!)
                                 }
-                                
+
                                 // Trigger a refresh if tokens are lost
                                 if !tokensExists && entity.isStoredOnDevice {
-                                    // TODO: Alert the user to revoke the platform or something.
+                                    showAlert = true
+                                    alertTitle = "Tokens Missing"
+                                    alertMessage = "Your tokens have not been found on this device. Please revoke access to your account and log back in to continue."
                                 }
                             } else {
                                 print("Platform is not stored on device")
                             }
-            
-                            encryptedFormattedContent = try messageComposer.textComposer(
-                                platform_letter: shortcode!,
-                                sender: fromAccount,
-                                text: textBody,
-                                accessToken: tokensExists ? storedTokenForPlatform?.accessToken : nil,
-                                refreshToken: tokensExists ? storedTokenForPlatform?.refreshToken : nil
-                            )
-                            
-                     
-                            print("Transmitting to sms app: \(encryptedFormattedContent)")
-                            
+
+                            encryptedFormattedContent =
+                                try messageComposer.textComposer(
+                                    platform_letter: shortcode!,
+                                    sender: fromAccount,
+                                    text: textBody,
+                                    accessToken: tokensExists
+                                        ? storedTokenForPlatform?.accessToken
+                                        : nil,
+                                    refreshToken: tokensExists
+                                        ? storedTokenForPlatform?.refreshToken
+                                        : nil
+                                )
+
+                            print( "Transmitting to sms app: \(encryptedFormattedContent)")
+
                             isPosting = false
                             isShowingMessages.toggle()
                         } catch {
@@ -168,14 +197,96 @@ struct TextComposeView: View {
                     SMSComposeMessageUIView(
                         recipients: [defaultGatewayClientMsisdn],
                         body: $encryptedFormattedContent,
-                        completion: handleCompletion(_:))
+                        completion: handleCompletion(_:)
+                    )
                     .ignoresSafeArea()
                 }
             }
         })
-        
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text(alertTitle),
+                message: Text(alertMessage),
+                primaryButton: .destructive(
+                    Text("Revoke Account"),
+                    action: {
+                        isLoading.toggle()
+                        print("Atrempting to revoke account")
+                        let backgroundQueue = DispatchQueue( label: "revokeAccountQueue", qos: .background)
+
+                        backgroundQueue.async {
+                            do {
+                                let vault: Vault = Vault()
+                                let llt: String = try Vault.getLongLivedToken()
+                                let publisher = Publisher()
+
+                                let platformEntity = platforms.first {
+                                    $0.name == platformName
+                                }
+
+                                if let unwrappedPlatform = platformEntity {
+                                    print("platform is: \(unwrappedPlatform)")
+                                    print("Triggered revoking method")
+                                    let storedPlatformEntityToDelete = storedPlatforms.first {
+                                        $0.account == fromAccount
+                                    }
+                                    if let entityToDelete = storedPlatformEntityToDelete {
+                                        context.delete(entityToDelete)
+                                    }
+                                   
+
+                                    let result: Bool =
+                                        try publisher.revokePlatform(
+                                            llt: llt,
+                                            platform: unwrappedPlatform.name!,
+                                            account: fromAccount,
+                                            protocolType:unwrappedPlatform.protocol_type!
+                                        )
+
+                                    if result {
+                                        DispatchQueue.main.async {
+                                            do {
+                                                let llt = try Vault.getLongLivedToken()
+                                                var _ = try vault.refreshStoredTokens(llt: llt, context: context)
+                                     
+                                                try context.save()
+                                                print("Successfully revoked platform")
+                                                dismiss()
+                                    
+                                            } catch {
+                                                print(error)
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    print(
+                                        "Platform is null, so cant revoke"
+                                    )
+                                }
+                            } catch {
+                                print(
+                                    "Unable to revoke platform: \(error)"
+                                )
+                            }
+                        }
+                    }),
+                secondaryButton: .default(Text("Cancel")){
+                    showAlert.toggle()
+                })
+        }
+        .overlay {
+            if isLoading {
+                ZStack {
+                    Color.black
+                        .opacity(0.2)
+                        .edgesIgnoringSafeArea(.all)
+                    ProgressView("Revoking...")//.foregroundStyle(Color.white)
+                        .padding()
+                }
+            }
+        }
     }
-    
+
     func handleCompletion(_ result: MessageComposeResult) {
         switch result {
         case .cancelled:
@@ -192,7 +303,7 @@ struct TextComposeView: View {
                 messageEntities.subject = fromAccount
                 messageEntities.body = textBody
                 messageEntities.date = Int32(Date().timeIntervalSince1970)
-                
+
                 DispatchQueue.main.async {
                     do {
                         try context.save()
@@ -213,16 +324,16 @@ struct TextView_Preview: PreviewProvider {
     static var previews: some View {
         let container = createInMemoryPersistentContainer()
         populateMockData(container: container)
-        
+
         @State var message: Messages? = Messages(
             id: UUID(),
             subject: "Hello world",
-            data: "The scroll view displays its content within the scrollable content region. As the user performs platform-appropriate scroll gestures, the scroll view adjusts what portion of the underlying content is visible. ScrollView can scroll horizontally, vertically, or both, but does not provide zooming functionality.",
+            data:"The scroll view displays its content within the scrollable content region. As the user performs platform-appropriate scroll gestures, the scroll view adjusts what portion of the underlying content is visible. ScrollView can scroll horizontally, vertically, or both, but does not provide zooming functionality.",
             fromAccount: "@afkanerd",
             toAccount: "toAccount@gmail.com",
             platformName: "twitter",
             date: Int(Date().timeIntervalSince1970))
-        
+
         @State var globalDismiss = false
         @State var platformName = "twitter"
         return TextComposeView(platformName: $platformName, message: $message)
