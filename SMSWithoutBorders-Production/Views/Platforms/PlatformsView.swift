82fd2ee3fad6cbe214f75d64979f52f4abe70c73
Lewis <lewisnuikweh@gmail.com>
2025-04-29 13:50:49 +0100

    token storage: handle nil service_type in PlatformsView

--- SMSWithoutBorders-Production/Views/Platforms/PlatformsView.swift
+++ SMSWithoutBorders-Production/Views/Platforms/PlatformsView.swift
@@ -81,11 +81,11 @@ struct PlatformsView: View {
                                         composeNewMessageRequested: $composeNewMessageRequested,
                                         platformRequestType: $requestType,
                                         composeViewRequested: getBindingComposeVariable(
-                                            type: item.service_type!),
+                                            type: item.service_type ?? "Unknown"),
                                         parentRefreshRequested: $refreshRequested,
                                         requestedPlatformName: $requestedPlatformName,
                                         platform: item,
-                                        serviceType: getServiceType(type: item.service_type!),
+                                        serviceType: getServiceType(type: item.service_type ?? "Unknown"),
                                         callback: callback
                                     )
                                 }
